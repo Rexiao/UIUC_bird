f8252e93dafc3c4adb7e3ba7713ea46b28456b08
Ang Li <angl2@illinois.edu>
2018-11-26 22:41:28 -0600

    fix

---
+++ app/src/main/java/com/example/xr/flappy_bird/BitmapBank.java
@@ -0,0 +1,40 @@
+package com.example.xr.flappy_bird;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public class BitmapBank {
+
+    Bitmap background;
+
+    public BitmapBank(Resources res) {
+        background = BitmapFactory.decodeResource(res, R.drawable.);
+        background = scaleImage(background);
+    }
+
+    //Return background bitmap
+    public Bitmap getBackground(){
+        return background;
+    }
+
+    //Return background width
+    public int getBackgroundWidth(){
+        return background.getWidth();
+    }
+
+    //Return background height
+    public int getBackgroundHeight(){
+        return background.getHeight();
+    }
+
+    public Bitmap scaleImage(Bitmap bitmap){
+        float widthHeightRatio = getBackgroundWidth() / getBackgroundHeight();
+        /*
+        We'll multiply widthHeightRatio with screenHeight to get scaled width of the bitmap.
+        Then call createScaledBitmap() to create a new bitmap, scaled from an existing bitmap, when possible.
+         */
+        int backgroundScaledWidth = (int) widthHeightRatio * AppConstants.SCREEN_HEIGHT;
+        return Bitmap.createScaledBitmap(bitmap, backgroundScaledWidth, AppConstants.SCREEN_HEIGHT, false);
+    }
+}
--- app/src/main/java/com/example/xr/flappy_bird/MainActivity.java
+++ app/src/main/java/com/example/xr/flappy_bird/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.xr.flappy_bird;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppConstants.initialization(this.getApplicationContext());
+    }
+
+    public void startGame(View view){
+        //Log.i("ImageButton","clicked");
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
